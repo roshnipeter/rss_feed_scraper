b5368537a6eab361d3dab503a239d050f949a54c
Roshni Peter <roshumep@gmail.com>
2023-03-09 21:29:58 +0530

    removed unwanted methods

--- builder.py
+++ builder.py
@@ -22,27 +22,3 @@ def rss_feeder(feed_url):
 def generate_hash(feed):
     return hashlib.md5(json.dumps(feed).encode('UTF-8')).hexdigest()
 
-def write_to_es(feed, url, userId):
-    es_record = {
-        '@timestamp': datetime.now(),
-        'feedData' : json.dumps(feed),
-        'url' : url,
-        'userId': userId,
-        'marked' : False,
-
-    }
-    es_service.add_data("rss_feeds",es_record)
-
-def read_data(userId):
-    userId = userId
-    marked = False
-    result, count = es_service.get_audit_log(userId, marked)
-
-def main():
-    url = "http://www.nu.nl/rss/Algemeen"
-    feed = rss_feeder(url)
-    write_to_es(feed, url, userId='100A1')
-
-if __name__ == '__main__':
-    main()
-
