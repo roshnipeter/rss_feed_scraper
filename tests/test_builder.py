f01c97bff93add5dd4d630577a4f1cd6025c18e9
Roshni Peter <roshumep@gmail.com>
2023-03-13 19:31:24 +0530

    added test cases for builder

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,30 +1,31 @@
-# import sys
-# import os
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
-# import builder
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
+import builder
 
-# class TestBuilder:
+class TestBuilder:
     
-#     @classmethod
-#     def setup_class(cls):
-#         pass
+    @classmethod
+    def setup_class(cls):
+        pass
 
-#     @classmethod
-#     def teardown_class(cls):
-#         pass
+    @classmethod
+    def teardown_class(cls):
+        pass
         
-#     def test_rss_feeder(cls):
-#         hash_key, feeds = builder.rss_feeder('http://www.nu.nl/rss/Algemeen')
-#         assert isinstance(hash_key, str)
-#         assert isinstance(feeds, list)
+    def test_rss_feeder(cls):
+        hash_key, feeds = builder.rss_feeder('http://www.nu.nl/rss/Algemeen')
+        assert isinstance(hash_key, str)
+        assert isinstance(feeds, list)
 
-#         assert all(isinstance(entry, dict) for entry in feeds)
-#         assert all("title" in entry for entry in feeds)
-#         assert all("summary" in entry for entry in feeds)
-#         assert all("link" in entry for entry in feeds)
-#         assert all("published" in entry for entry in feeds)
+        assert all(isinstance(entry, dict) for entry in feeds)
+        assert all("title" in entry for entry in feeds)
+        assert all("summary" in entry for entry in feeds)
+        assert all("link" in entry for entry in feeds)
+        assert all("published" in entry for entry in feeds)
     
-#     def test_generate_hash(cls):
-#         hash_key = builder.generate_hash('http://www.nu.nl/rss/Algemeen')
-#         assert hash_key == '489dd89f4b0474b26f5247c2fa5257f7'
+    def test_generate_hash(cls):
+        hash_key = builder.generate_hash('http://www.nu.nl/rss/Algemeen')
+        assert hash_key == '489dd89f4b0474b26f5247c2fa5257f7'
+
 
